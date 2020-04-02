10710c4ae3ff86e462256f433588692238f85980
liamst19 <50284753+liamst19@users.noreply.github.com>
2020-04-02 15:57:08 +0900

    4: calculator project passes all tests

--- 4-javascript-calculator/src/components/CalculatorDisplay.jsx
+++ 4-javascript-calculator/src/components/CalculatorDisplay.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const CalculatorDisplay = ({ calculator }) => {
-  return (<div id="display">
-            { calculator.join('') || '0'}
+  return (<div id="display-panel">
+            <div id="display-operator">&nbsp;{ calculator.operator }</div>
+            <div id="display-term">&nbsp;<span id="display">{ calculator.term2 ? calculator.term2 : calculator.term1 }</span></div>
           </div>)
 }
 
--- 4-javascript-calculator/src/reducers/calcReducer.js
+++ 4-javascript-calculator/src/reducers/calcReducer.js
@@ -1,31 +1,88 @@
-import calculate, { arithOperation } from '../services/calculator'
+import { arithOperation } from '../services/calculator'
 
-const appendChar = (state, char) => {
-      const OPRX = /[-+*/]/;
-      const last = state[state.length - 1];
-      return (
-        char === '.' ? state
-          : char === '0' && state[0] === '0' && state.length === 1 ? []
-          : OPRX.test(last) && OPRX.test(char) ?
-          char === '-' ? [...state, char]
-          : [...state.slice(0, state.length - 1), char]
-        : [...state, char]
-      )
+const DEFAULT_STATE = {
+  operator: '',
+  term1: '0',
+  term2: ''
 }
 
-const reducer = (state = [], action) => {
+const appendChar = (state, char) => {
+
+  const OPRX = /^[-+*/]$/;
+
+  const doArith = ({operator, term1, term2}) => {
+    console.log('doing arith');
+    return arithOperation[operator](term1, term2)
+      .toPrecision(15).replace(/(?:\.0+|(\.\d+?)0+)$/, "$1");
+  }
+
+  const appendToTerm = (term, char) => {
+    return (
+      char === '.' ? // ----- decimal
+        term.length === 0 ? '0.'
+        : term === '0' ? '0.'
+        : term === '-' ? '-0.'
+        : /\./.test(term) ? term : term.concat('.')
+      // ---- numeric
+      : term === '0' ? char : term.concat(char)
+    )
+  }
+
+  const appendToOperator = (state, char) => {
+    return (
+      char === '-' ? // subtraction or minus ----------
+        state.term2.length === 0 ? // if there's no term2
+        // --- set term1
+      (/^0?\.?$/.test(state.term1)) ? // if term1 empty or zero
+        { ...state, term1: '-'.concat(state.term1) }
+      // if term1 has term
+      : state.operator ? // -- * if there is already an operator
+        {...state, term2: '-' } // -- second term will be negative
+      : {...state, operator: '-' } // replace operator
+      // -- if there is term 2
+      : state.term2 === '-' ? { ...state, term2: '' }
+        : { term1: doArith(state),
+          term2: '',
+          operator: char }
+      // char is not subtraction ----------
+      : state.term2 ?
+        state.term2 === '-' ? { ...state, term2: '', operator: char }
+        : { term1: doArith(state),
+                        term2: '',
+                        operator: char }
+      : state.term1 ? { ...state,
+                        operator: char} // replace operator
+      : state
+    )
+  }
+
+  const newState =
+        // if equal sign calculate and reset
+        char === '=' ?
+        state.term1 && state.term2 && state.operator ?
+        {...DEFAULT_STATE, term1: doArith(state)}
+        : {DEFAULT_STATE, ...state}
+  // ---- if char is operator
+        : OPRX.test(char) ? appendToOperator(state, char)
+  // ---- if char is numeric or decimal
+        : OPRX.test(state.operator) ? // does state have an operator?
+        { ...state, term2: appendToTerm(state.term2, char)}
+        : { ...state, term1: appendToTerm(state.term1, char)};
+  return newState;
+}
+
+const reducer = (state = DEFAULT_STATE, action) => {
   const switcher = {
-    'CALC_CLEAR': () => [],
+    'CALC_CLEAR': () => DEFAULT_STATE,
     'CALC_APPEND': () => appendChar(state, action.char),
-    'CALC_CALCULATE': () => calculate(state).toString().split('')
+    'CALC_CALCULATE': () => state.term1
   }
   return switcher.hasOwnProperty(action.type) ?
     switcher[action.type]() : state;
 }
 
 export const pushKey = key => {
-  return key === '=' ? calculateStore()
-    : key === 'C' ? clearCalculator()
+  return key === 'C' ? clearCalculator()
     : appendCalculator(key);
 }
 
